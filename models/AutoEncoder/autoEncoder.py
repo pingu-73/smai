777251b216a8f64b188ea56ad1269d789ea30bea
Dikshant <dikshant.073@gmail.com>
2024-10-13 23:44:45 +0530

    --allow-empty-message
    Signed-off-by: Dikshant <dikshant.073@gmail.com>

--- models/AutoEncoder/autoEncoder.py
+++ models/AutoEncoder/autoEncoder.py
@@ -4,25 +4,28 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../'
 from models.mlp.mlp_regression import MLPRegression
 
 class AutoEncoder:
-    def _init_(self, input_size, latent_size=6, learning_rate=0.001, max_iter=1000, batch_size=32, tol=1e-4):
-        self.input_size = input_size
-        self.latent_size = latent_size
+    def __init__(self, input_dim, latent_dim, hidden_layer_sizes=(100,), activation='relu',
+                 learning_rate=0.001, max_iter=1000, tol=1e-4, batch_size=32):
+        self.input_dim = input_dim
+        self.latent_dim = latent_dim
         
-        self.model = MLPRegression(hidden_layer_sizes=(latent_size,), activation='relu', 
-                                   learning_rate=learning_rate, max_iter=max_iter, 
-                                   batch_size=batch_size, tol=tol)
+        # Encoder: input_dim -> hidden layer(s) -> latent_dim
+        self.encoder = MLPRegression(hidden_layer_sizes=hidden_layer_sizes + (latent_dim,),
+                                      activation=activation, learning_rate=learning_rate,
+                                      max_iter=max_iter, tol=tol, batch_size=batch_size)
+        
+        # Decoder: latent_dim -> hidden layer(s) -> input_dim
+        self.decoder = MLPRegression(hidden_layer_sizes=(latent_dim,) + hidden_layer_sizes[::-1],
+                                      activation=activation, learning_rate=learning_rate,
+                                      max_iter=max_iter, tol=tol, batch_size=batch_size)
 
     def fit(self, X):
-        print("Training the AutoEncoder...")
-        self.model.fit(X, X)
+        latent_representation = self.encoder.fit(X, X).predict(X)
+        self.decoder.fit(latent_representation, X)
 
     def get_latent(self, X):
-        self.model._initialize_weights(X.shape[1])
-        self.model._forward(X)
-        
-        latent_representation = self.model.activations[1]
-        return latent_representation
+        return self.encoder.predict(X)
 
     def reconstruct(self, X):
-        reconstructed_X = self.model.predict(X)
-        return reconstructed_X
+        latent_representation = self.get_latent(X)
+        return self.decoder.predict(latent_representation)
